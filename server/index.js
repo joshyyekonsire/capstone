008040e0a4aeb48d01fd1cd0eecae68adba739d6
Daniel Brown <daniel.brown@devmounta.in>
2023-02-06 23:48:36 -0700

    bitIo set up

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.use(cors());
 
 const baseURL = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/";
 
+// const bitIo = 'postgresql://cosmicblaze123:v2_3yqRy_A6TS8PbDYRSUr6WVURnvyiB@db.bit.io:5432/cosmicblaze123/capstone-project'
+
+
+
 app.get("/stock/:ticker", (req, res) => {
   axios
     .get(
@@ -17,7 +21,5 @@ app.get("/stock/:ticker", (req, res) => {
     .then((response) => res.send(response.data))
     .catch((error) => res.status(404).send({ message: "Ticker not found" }));
 });
-
-
 
 app.listen(4455, () => console.log("Casting off from PORT: 4455"));
