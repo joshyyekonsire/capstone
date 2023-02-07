6edc4e66278a70929b1fd52b42a8fa432e45e9da
Daniel Brown <daniel.brown@devmounta.in>
2023-02-06 22:37:23 -0700

    test folder

---
+++ client/__tests__/test.js
@@ -0,0 +1,19 @@
+// functions to test:
+
+// update time
+
+// update ny time
+
+// submit query
+
+// addCard
+
+// refresh
+
+// sell all 
+
+// create portfolio
+
+// savePortfolio
+
+// search for company info
